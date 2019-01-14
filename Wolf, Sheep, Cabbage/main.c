b6f7a3967770f4cd94b37169c7dc12b448296681
m-luketin <ml46019@oss.unist.hr>
2019-01-14 14:42:48 +0100

    Commit message

--- Wolf, Sheep, Cabbage/main.c
+++ Wolf, Sheep, Cabbage/main.c
@@ -88,11 +88,130 @@ int GetMoves(State beginningState, int iteration)
 	return 0;
 }
 
+// drugi dio
+typedef struct _QueueState
+{
+	int wolf;
+	int sheep;
+	int cabbage;
+	int boat;
+	struct _QueueState* prevState;
+}QueueState;
+
+typedef struct
+{
+	QueueState stateList[1000];
+	int head;
+	int tail;
+	int filled;
+}Queue;
+
+void CreateQueueChildren(QueueState parentState, Queue queue)
+{
+	if (parentState.wolf == parentState.boat) {
+		queue.stateList[queue.tail].wolf = !parentState.wolf;
+		queue.stateList[queue.tail].boat = !parentState.boat;
+		queue.stateList[queue.tail].prevState = &parentState;
+		queue.filled++;
+		queue.tail++;
+	}
+	if (parentState.sheep == parentState.boat) {
+		queue.stateList[queue.tail].sheep = !parentState.sheep;
+		queue.stateList[queue.tail].boat = !parentState.boat;
+		queue.stateList[queue.tail].prevState = &parentState;
+		queue.tail++;
+	}
+	if (parentState.cabbage == parentState.boat) {
+		queue.stateList[queue.tail].cabbage = !parentState.cabbage;
+		queue.stateList[queue.tail].boat = !parentState.boat;
+		queue.stateList[queue.tail].prevState = &parentState;
+		queue.filled++;
+		queue.tail++;
+	}
+	queue.stateList[queue.tail].boat = !parentState.boat;
+	queue.stateList[queue.tail].prevState = &parentState;
+	queue.filled++;
+	queue.tail++;
+}
+
+void QueuePrintState(QueueState currentGameState)
+{
+	printf("W S C B:");
+	if (currentGameState.wolf)
+		printf("1");
+	else
+		printf("0");
+
+	if (currentGameState.sheep)
+		printf("1");
+	else
+		printf("0");
+
+	if (currentGameState.cabbage)
+		printf("1");
+	else
+		printf("0");
+
+	if (currentGameState.boat)
+		printf("1");
+	else
+		printf("0");
+
+	printf("\n");
+}
+
+int QueueWinState(QueueState state)
+{
+	return (state.wolf && state.sheep && state.cabbage);
+}
+
+int QueueLoseState(QueueState state)
+{
+	return((state.sheep == state.wolf && state.sheep != state.boat) ||
+		(state.cabbage == state.sheep && state.cabbage != state.boat));
+}
+
+int QueueStartState(QueueState state)
+{
+	return(!state.sheep && !state.wolf && !state.cabbage);
+}
+
+void GetQueueMoves(Queue queue)
+{
+	QueueState tmpState;
+	int i = 0;
+	while (!QueueWinState(queue.stateList[i])) {
+		i++;
+	}
+	tmpState = queue.stateList[i];
+	while (!QueueStartState(tmpState))
+	{
+		QueuePrintState(tmpState);
+		tmpState = *tmpState.prevState;
+	}
+}
+
 void main()
 {
-	State startState = { 0 };
-	for(int i = 0; !GetMoves(startState, i); i++)
-		GetMoves(startState, i);
+	//prvi dio
+	
+	QueueState startState = { 0 };
+
+	//for(int i = 0; !GetMoves(startState, i); i++)
+		//GetMoves(startState, i);
+	
+	//drugi dio
+	Queue newQueue;
+	newQueue.head = 0;
+	newQueue.stateList[0] = startState;
+	newQueue.tail = 1;
+	newQueue.filled = 1;
+
+	for (; !QueueWinState(newQueue.stateList[newQueue.tail]); newQueue.head++) {
+		CreateQueueChildren(newQueue.stateList[newQueue.head], newQueue);
+	}
+
+	GetQueueMoves(newQueue);
 
 	system("pause");
 }
